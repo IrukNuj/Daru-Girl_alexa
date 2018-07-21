064658fe27d24f5d805e26901660ebe4709eedd5
IrukNuj <b1716568@planet.kanazawa-it.ac.jp>
2018-07-22 00:56:49 +0900

    dotenv

--- intents/test.rb
+++ intents/test.rb
@@ -6,10 +6,10 @@ end
 
 intent "GetNewIntent" do
   client = Twitter::REST::Client.new do |config|
-    config.consumer_key = ENV[CONSUMER_KEY]
-    config.consumer_secret = ENV[CONSUMER_SECRET]
-    config.access_token = ENV[ACCESS_TOKEN]
-    config.access_token_secret = ENV[ACCESS_TOKEN_SECRET]
+    config.consumer_key = ENV['CONSUMER_KEY']
+    config.consumer_secret = ENV['CONSUMER_SECRET']
+    config.access_token = ENV['ACCESS_TOKEN']
+    config.access_token_secret = ENV['ACCESS_TOKEN_SECRET']
   end
 
   tweets = []
